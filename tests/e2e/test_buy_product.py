2d0af65c86a76f292e428258de423430d7b6443c
tamerassi <108218381+tamerassi@users.noreply.github.com>
2022-08-28 10:07:40 +0300

    Update test_buy_product.py

--- tests/e2e/test_buy_product.py
+++ tests/e2e/test_buy_product.py
@@ -44,7 +44,7 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from webdriver_manager.chrome import ChromeDriverManager
+
 
 
 @pytest.fixture()
