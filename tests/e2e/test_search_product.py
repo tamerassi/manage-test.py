31428e5447bf86064b3c6cdc0520a12ea1d9d49d
Mohamad <mhmdassi@hotmail.com>
2022-08-28 10:21:03 +0300

    change

--- tests/e2e/test_search_product.py
+++ tests/e2e/test_search_product.py
@@ -3,7 +3,6 @@ import time
 import pytest
 
 from selenium import webdriver
-from selenium.webdriver import Keys
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options as FireFoxOptions
--- tests/e2e/test_user.py
+++ tests/e2e/test_user.py
@@ -1,10 +1,8 @@
-
 import time
 
 import pytest
 
 from selenium import webdriver
-from selenium.webdriver import Keys
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options as FireFoxOptions
@@ -13,6 +11,7 @@ from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
 
 @pytest.fixture()
 def driver():
@@ -27,6 +26,7 @@ def driver():
 testing user
 
 '''
+
 
 def test_user_login(driver):
     driver.get("http://127.0.0.1:8000/#/")
@@ -96,40 +96,6 @@ def test_user_create_account_for_existed_account(driver):
                                      "#root > div > main > div > div > div > div.fade.alert.alert-danger.show").text
     assert error_note == "User with this email is already registered"
 
-#
-# def test_user_invalid_username(driver):
-#     driver.get("http://127.0.0.1:8000/#/")
-#     driver.find_element(By.CSS_SELECTOR, ".nav-link:nth-child(2)").click()
-#     driver.find_element(By.ID, "email").click()
-#     driver.find_element(By.ID, "email").send_keys("tamer.tester@gmail.com")
-#     driver.find_element(By.ID, "password").click()
-#     driver.find_element(By.ID, "password").send_keys("assi12345")
-#     driver.find_element(By.CSS_SELECTOR, ".mt-3").click()
-#     time.sleep(2)
-#     driver.execute_script(
-#         "document.querySelector('#root > div > main > div > div > div > div.fade.alert.alert-danger.show').scrollIntoView();")
-#     error_note = driver.find_element(By.CSS_SELECTOR,
-#                                      "#root > div > main > div > div > div > div.fade.alert.alert-danger.show").text
-#     assert error_note == "No active account found with the given credentials"
-
-#
-# def test_user_incorrect_password(driver):
-#     driver.get("http://127.0.0.1:8000/#/")
-#     driver.maximize_window()
-#     driver.find_element(By.CSS_SELECTOR, ".nav-link:nth-child(2)").click()
-#     driver.find_element(By.ID, "email").click()
-#     driver.find_element(By.ID, "email").send_keys("tamer.tester@gmail.com")
-#     driver.find_element(By.ID, "password").click()
-#     driver.find_element(By.ID, "password").send_keys("assi12345")
-#     time.sleep(5)
-#     driver.find_element(By.CSS_SELECTOR, ".mt-3").click()
-#     time.sleep(2)
-#     driver.execute_script(
-#         "document.querySelector('#root > div > main > div > div > div > div.fade.alert.alert-danger.show').scrollIntoView();")
-#     error_note = driver.find_element(By.CSS_SELECTOR,
-#                                      "#root > div > main > div > div > div > div.fade.alert.alert-danger.show").text
-#     assert error_note == "No active account found with the given credentials"
-#
 
 def test_user_create_account_with_incorrect_email_format(driver):
     driver.get("http://127.0.0.1:8000/#/")
@@ -153,31 +119,3 @@ def test_user_create_account_with_incorrect_email_format(driver):
     time.sleep(2)
     # driver.execute_script("window.scrollTo(0,198.6666717529297)")
     assert driver.current_url == "http://127.0.0.1:8000/#/register?redirect=/"
-
-
-'''
-testing admin user
-
-'''
-
-
-# @pytest.mark.parametrize("validation", ["TAMERASSI1", "ADMIN"])
-# def test_admin_user_login(driver, validation):
-#     driver.get("http://127.0.0.1:8000/#/")
-#     driver.maximize_window()
-#     driver.find_element(By.CSS_SELECTOR, ".nav-link:nth-child(2)").click()
-#     # time.sleep(2)
-#     driver.find_element(By.ID, "email").click()
-#     driver.find_element(By.ID, "email").send_keys("tamerassi1@gmail.com")
-#     driver.find_element(By.ID, "password").click()
-#     driver.find_element(By.ID, "password").send_keys("assi12345")
-#     driver.find_element(By.CSS_SELECTOR, ".mt-3").click()
-#     time.sleep(5)
-#     username = driver.find_element(By.CSS_SELECTOR, "#username").text
-#     time.sleep(3)
-#     admin = driver.find_element(By.CSS_SELECTOR, "#adminmenu").text
-#     time.sleep(3)
-#     names = [username, admin]
-#     assert validation in names
-#
-
